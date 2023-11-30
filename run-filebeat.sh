5065af848e317b9b1e1ec2ab9b12f6bd9356f9b8
Ivan <ivan.zderadicka@gmail.com>
2023-11-30 12:41:47 +0100

    TLS pro komunikaci filebeat -> logstash

--- run-filebeat.sh
+++ run-filebeat.sh
@@ -12,6 +12,9 @@ echo staring filebeat v${VERSION}
 
 $SUDO podman run --rm --name filebeat --net host \
     -v "./filebeat/filebeat.yml:/usr/share/filebeat/filebeat.yml:ro,Z" \
+    -v "./tls/certs/ca/ca.crt:/usr/share/filebeat/ca.crt:ro,z" \
+    -v "./tls/certs/filebeat/filebeat.crt:/usr/share/filebeat/filebeat.crt:ro,Z" \
+    -v "./tls/certs/filebeat/filebeat.key:/usr/share/filebeat/filebeat.key:ro,Z" \
     -v "$LOGS:/logs:ro,z" \
     -v "filebeat-registry:/usr/share/filebeat/data:rw" \
     docker.elastic.co/beats/filebeat-oss:${VERSION} \
